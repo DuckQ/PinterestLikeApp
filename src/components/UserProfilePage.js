ef324b7c9335cbaf98c015d40a054c9c2adff313
DuckQ <zrexzqq@gmail.com>
2018-04-18 19:09:22 +0300

    added conditional rendering for UserProfilePage

--- src/components/UserProfilePage.js
+++ src/components/UserProfilePage.js
@@ -3,19 +3,32 @@ import { connect } from 'react-redux';
 import { updateUserInfo } from '../redux/actions/updateUserInfo';
 import UpdateUserInfoForm from './UpdateUserInfoForm';
 import { logout, setCurrentUser } from '../redux/actions/authActions';
+import PageNotFound from './PageNotFound';
 
 class UserProfilePage extends Component {
   constructor(props) {
     super(props);
-  
+
+  this.state = {
+      isUserLegit: null
+    }
   };
   
+  componentWillMount() {
+    const userId = this.props.location.pathname.split("/user/")[1];
+    if (userId === this.props.auth.user.username){
+      this.setState({isUserLegit: true})
+    } else {
+      this.setState({isUserLegit: false})
+    };
+  };
+
   render() {
     const { user } = this.props.auth;
 
     return (
       <div>
-        <UpdateUserInfoForm setCurrentUser={this.props.setCurrentUser} updateUserInfo={this.props.updateUserInfo} user={user} logout={this.props.logout} />
+        { this.state.isUserLegit ? <UpdateUserInfoForm setCurrentUser={this.props.setCurrentUser} updateUserInfo={this.props.updateUserInfo} user={user} logout={this.props.logout} /> : <PageNotFound />}
       </div>
     )
   }
