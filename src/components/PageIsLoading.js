ddb99596d2b68a1577255d08c307bbf9ffa89976
DuckQ <zrexzqq@gmail.com>
2018-05-02 17:30:06 +0300

    changed file path

--- src/components/PageIsLoading.js
+++ src/components/PageIsLoading.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class PageIsLoading extends Component {
   render() {
     return (
+      // heroku and local machine use diffent pathes
       <div>
-        <img src="../files/loading.svg" alt=""/>
+        <img src="/src/files/loading.svg" alt=""/>
+        <img src="/files/loading.svg" alt=""/>
       </div>
     )
   }
